a6edacd4a980f2bd240135182f204c5550d09cc5
Aidan Dennehy <duinneacha@gmail.com>
2018-09-08 12:31:36 +0100

    add new message and create message events

---
+++ public/js/index.js
@@ -0,0 +1,25 @@
+var socket = io();
+
+// Listen for an event on connect
+socket.on('connect', function () {
+  console.log('Connected to the server');
+
+
+  socket.emit('createMessage', {
+    from: "Aidan",
+    text: "Message successfully transmitted"
+  });
+
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+
+});
+
+
+
--- server/server.js
+++ server/server.js
@@ -21,6 +21,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected')
 
+
+  socket.emit('newMessage', {
+    from: 'localHost',
+    text: 'Message from the middle',
+    createdAt: 324324
+  })
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
