d3f2583fd5cfa99840ba5f052f25a14c86dcf6c9
Aidan Dennehy <duinneacha@gmail.com>
2018-09-08 09:24:08 +0100

    Add connectivity handlers

--- server/server.js
+++ server/server.js
@@ -1,20 +1,33 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, './../public');
-const serverPath = path.join(__dirname, './../server');
-
 const express = require('express');
+const socketIO = require('socket.io');
+
 
 const port = process.env.PORT || 3000;
 const app = express();
+var server = http.createServer(app);
 
 
+// Create the websockets server
+var io = socketIO(server);
 
 // Let node know the static folder
 app.use(express.static(publicPath));
 
 
+// Listen for a connection
+io.on('connection', (socket) => {
+  console.log('New user connected')
 
-app.listen(port, () => {
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+
+server.listen(port, () => {
   console.log(`Started up at port ${port}`);
 });
 
