63f718ee5d98de448df5efa86fc783c3377a14db
Aidan Dennehy <duinneacha@gmail.com>
2018-09-11 19:08:16 +0100

    Filled in user get, getall and removeuser functions

--- server/utils/users.js
+++ server/utils/users.js
@@ -11,6 +11,36 @@ class Users {
     this.users.push(user);
     return user;
   }
+
+  removeUser(id) {
+    // return user that was removed
+    const user = this.getUser(id);
+
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+
+  }
+
+  getUser(id) {
+    // Get user
+    return this.users.filter((user) => user.id === id)[0];
+  }
+
+  getUserList(room) {
+
+    // return array filtering only those in the room requested
+    const users = this.users.filter((user) => user.room === room);
+
+    // take the array of objects and convert to an array of strings
+    const namesArray = users.map((user) => user.name);
+
+    return namesArray;
+
+  }
+
+
 }
 
 
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,7 +2,38 @@ const expect = require('expect');
 const { Users } = require('./users');
 
 
-describe('Users', () => {
+describe('Users Tests', () => {
+
+  var users;
+
+
+  beforeEach(() => {
+
+
+
+    users = new Users();
+    users.users = [{
+      id: '1',
+      name: 'Dolly Parton',
+      room: 'Country Music Legends'
+    },
+    {
+      id: '2',
+      name: 'Johnny Cash',
+      room: 'Country Music Legends'
+    },
+    {
+      id: '3',
+      name: 'Dick Pomphret',
+      room: 'Port Users'
+    },
+    {
+      id: '4',
+      name: 'Mike Blethley',
+      room: 'Port Users'
+    }];
+
+  });
 
 
   it('should add new user', () => {
@@ -12,7 +43,53 @@ describe('Users', () => {
       name: 'Aidan',
       room: 'Dolly Parton Fans'
     };
+
+
     const resUser = testUsers.addUser(user.id, user.name, user.room);
     expect(testUsers.users).toEqual([user]);
+  });
+
+  // Testing getUser
+  it('should find a user', () => {
+    const idToFind = '2';
+    const retUser = users.getUser(idToFind);
+
+    expect(retUser.id).toBe(idToFind);
+  });
+
+  it('should not find a user', () => {
+    const idToFind = 'asd';
+    const retUser = users.getUser(idToFind);
+
+    expect(retUser).toBeFalsy();
+
+  });
+
+  // Testing removerUser
+  it('should remove a user', () => {
+    const idToFind = '4';
+    const retUser = users.removeUser(idToFind);
+
+    expect(retUser.id).toBe(idToFind);
+    expect(users.users.length).toBe(3);
+
+  });
+
+  it('should not remove a user', () => {
+    const idToFind = '34352';
+    const retUser = users.removeUser(idToFind);
+
+    expect(retUser).toBeFalsy();
+    expect(users.users.length).toBe(4);
+
+  });
+
+
+  // Testing getUserList
+  it('should return names for the Country Music Legends Club', () => {
+
+    var userList = users.getUserList('Country Music Legends');
+
+    expect(userList).toEqual(['Dolly Parton', 'Johnny Cash'])
   });
 });
